153de72da83185f007cb9bc36d09747697015167
Tarandeep Mittal <33363377+tdmittens@users.noreply.github.com>
2021-05-08 22:51:28 -0400

    Added navbar

---
+++ src/Components/navbar.svelte
@@ -0,0 +1,47 @@
+<script>
+
+</script>
+
+
+<main>
+    <div class="navbar-container">
+        <div class="navbar-logo">
+            <h4>Tarandeep Mittal</h4>
+        </div>
+        <ul class="navbar-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Music</a></li>
+            <li><a href="/">Projects</a></li>
+        </ul>
+    </div>
+</main>
+
+
+<style>
+    .navbar-container {
+        display:flex;
+        justify-content: space-around;
+        align-items: center;
+        min-height: 10vh;
+        background-color: #1c1b22;
+    }
+    .navbar-logo {
+        color: white;
+        text-transform: uppercase;
+        letter-spacing: 5px;
+    }
+    .navbar-links {
+        display:flex;
+        justify-content: space-around;
+        width: 30%;
+        list-style-type: none;        
+    }
+    .navbar-links a {
+        color: white;
+        text-decoration: none;
+        letter-spacing: 3px;
+        font-weight: bold;
+        font-size: 14px;
+    }
+</style>
