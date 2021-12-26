b078abb92ae9410d347afb6ade63c43fe74187c0
Tarandeep Mittal <33363377+tdmittens@users.noreply.github.com>
2021-12-26 11:01:56 -0500

    updated music playlists

--- src/Components/Music.svelte
+++ src/Components/Music.svelte
@@ -5,23 +5,23 @@
 
 <main>
     <Navbar/>
-    <h1>Still updating!</h1>
     <div class="music-container">
         <div class="music-section">
             <h2>Top 10 Songs</h2>
+            
             <iframe src="https://open.spotify.com/embed/playlist/2KQsKqJPdzB5dd13yOD0dE" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Top 10 Songs"></iframe>
         </div>
         <div class="music-section">
-            <h2>Korean Rotation</h2>
-            <iframe src="https://open.spotify.com/embed/playlist/1GoyVE3X1hOiLIQTQYKgsw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Korean Rotation"></iframe>
+            <h2>Rotation</h2>
+            <iframe src="https://open.spotify.com/embed/playlist/1R4p6oyJcvsR5EJ9PXB0Jv" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Korean Rotation"></iframe>
         </div>
         <div class="music-section">
-            <h2>English Rotation</h2>
-            <iframe src="https://open.spotify.com/embed/playlist/4nHTuByk8YV1H2sRkOnatd" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="English Rotation"></iframe>
+            <h2>K-POP</h2>
+            <iframe src="https://open.spotify.com/embed/playlist/4ghWNkDFPjqhVPayTwu7j9" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="English Rotation"></iframe>
         </div>
         <div class="music-section">
             <h2>Favourite Mix</h2>
-            <iframe src="https://open.spotify.com/embed/playlist/3EwVOzbLnNGsiPeb4NyDyI" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Current Favourite Mix"></iframe>
+            <iframe src="https://open.spotify.com/embed/playlist/0Kf8LFRaCB6PMPDkiXveNG" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Current Favourite Mix"></iframe>
         </div>
     </div>
 </main>
