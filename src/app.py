3e3bfdc44410b4f40a51dcf12baac91367957462
Tarandeep Mittal <33363377+tdmittens@users.noreply.github.com>
2022-02-20 13:55:30 -0500

    Additional fixes for flask-freeze

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home/")
 def home():
     return render_template('home.html', title="Home")
 
--- src/freeze.py
+++ src/freeze.py
@@ -3,5 +3,23 @@ from app import app
 
 freezer = Freezer(app)
 
+
+FREEZER_IGNORE_MIMETYPE_WARNINGS = True
+FREEZER_REMOVE_EXTRA_FILES = False
+
+freezer = Freezer(app, with_static_files=True,
+                  log_url_for=False, with_no_argument_rules=False)
+
+
+@freezer.register_generator
+def url_generator():
+    yield '/'
+    yield '/music'
+    yield '/resume'
+
+
 if __name__ == '__main__':
     freezer.freeze()
+
+# because i did dumb things
+# https://code.luasoftware.com/tutorials/flask/generate-static-html-with-flask-frozen/
